a903878c4f6595780a5382788f47706ac17a3f3a
addha <85791287+addha@users.noreply.github.com>
2021-09-15 13:22:43 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+//https://github.com/pro-whitehatjr/C18_TA_solution/blob/main/sketch.js   
+//CLASE PRO17 COMO BAJAR PRIMER PROYECTO EN GITHUB Y  ARREGLOS
+
+var balls = []; 
+var gravity = 0.1;
+
+function setup() {
+  createCanvas(480, 240);
+  balls.push(new Ball(150,0,20));
+}
+
+function draw() {
+  background(178, 217, 48);
+  
+  for(var x = 0; x < balls.length; x ++ ) { 
+    balls[x].update();
+    balls[x].display();
+  }
+}
+
+function mousePressed() {
+  var b = new Ball(mouseX,mouseY,20);
+  balls.push(b);
+}
+
